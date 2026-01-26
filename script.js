7b6fdb85a83d873c1d2f59419c29c0de97733235
Muhamed Elockly <elocklymuhamed@gmail.com>
2026-01-26 20:00:50 +0200

    Style CV link as nav-link to match other navbar elements

--- script.js
+++ script.js
@@ -1021,7 +1021,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('#navbarNav');
     const navLinks = document.querySelectorAll('.nav-link[href^="#"]');
-    const cvButton = document.querySelector('.navbar-nav .btn[href*=".pdf"]');
+    const cvLink = document.querySelector('.nav-link[href*=".pdf"]');
     
     // Close navbar when clicking on a nav link
     navLinks.forEach(link => {
@@ -1037,9 +1037,9 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
     
-    // Close navbar when clicking on CV button
-    if (cvButton) {
-        cvButton.addEventListener('click', function() {
+    // Close navbar when clicking on CV link
+    if (cvLink) {
+        cvLink.addEventListener('click', function() {
             if (navbarCollapse.classList.contains('show')) {
                 const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
                     toggle: false
